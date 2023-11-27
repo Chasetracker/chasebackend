fd946bcdef6c419499cd54dada1ee7e16800a5a5
Onuoha Collins <onuohacollinschidubem@gmail.com>
2023-11-27 11:00:49 +0100

    new chages

--- routes/customers.js
+++ routes/customers.js
@@ -9,6 +9,7 @@ const router = express.Router();
 
 //signup at /api/user/signup
 router.post('/add-customer', verify, AddCustomer);
+//get all customers at /api/users/all
 router.get('/customers', verify, getCustomers)
 
 
